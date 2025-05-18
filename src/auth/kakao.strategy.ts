d1fb7a6fca6dfe77c65ebd9eff54082ef5d9ecde
afraca130 <zokatn@gmail.com>
2025-05-18 22:23:17 +0900

    feat: kakako 전략 수정

---
+++ src/auth/kakao.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-kakao';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
+  constructor(private usersService: UsersService) {
+    super({
+      clientID: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      callbackURL: process.env.KAKAO_CALLBACK_URL,
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any) {
+    const { id, username, _json } = profile;
+    const user = await this.usersService.findOrCreateKakaoUser({
+      kakaoId: id.toString(),
+      email: _json.kakao_account?.email,
+      name: username,
+    });
+    return user;
+  }
+}
