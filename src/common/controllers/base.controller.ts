718bb9374f916a490a5f85f41cfa358682dd95cb
afraca130 <zokatn@gmail.com>
2025-05-22 18:46:19 +0900

    feat: common 폴더 수정

--- src/common/controllers/base.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
-
-@ApiBearerAuth()
-export abstract class BaseController {
-  constructor(protected readonly service: any) {}
-}
